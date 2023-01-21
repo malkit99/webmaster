ee59a8bd9a1ef9e62127ac5858303a43b9b4285c
RGCSM <rgcsm.pb@gmail.com>
2023-01-21 09:50:02 +0530

    first

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import './bootstrap';
+
+import { createApp } from "vue";
+import welcome from "./views/welcome.vue";
+
+createApp(welcome).mount("#app");
---
+++ resources/js/views/welcome.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Macroword computer academy macroword online</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
